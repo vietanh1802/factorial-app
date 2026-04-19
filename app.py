14f6024fd1e6e1d285f8b8b756e664377652bd2c
Viet Anh <vuducvietanh1802@gmail.com>
2026-04-19 17:01:16 +0700

    fix app.py

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ import streamlit as st
 from factorial import fact
 
 
-st.title ("Ứng dụng tính toán cơ bản")
+# st.title("Ứng dụng tính toán cơ bản")
 
-# Đặt tiêu đề phụ
-st.header("Tính giai thừa của một số tự nhiên")
+# # Đặt tiêu đề phụ
+# st.header("Tính giai thừa của một số tự nhiên")
 
-# Đoạn mô tả
-st.write("Ứng dụng này cho phép bạn tính giai thừa của một số tự nhiên. Vui lòng nhập số cần tính giai thừa vào ô dưới đây và nhấn nút 'Calculate'.")
+# # Đoạn mô tả
+# st.write("Ứng dụng này cho phép bạn tính giai thừa của một số tự nhiên. Vui lòng nhập số cần tính giai thừa vào ô dưới đây và nhấn nút 'Calculate'.")
 
 def main():
-    st.title ("Factorial Calculator")
-    number = st.number_input ("Enter a number :", min_value = 0, max_value = 900)
+    st.title("Factorial Calculator")
+    number = st.number_input("Enter a number :", min_value = 0, max_value = 900)
 
     if st.button("Calculate"):
         result = fact(number)
-        st.write (f"The factorial of {number} is {result}")
+        st.write(f"The factorial of {number} is {result}")
 
 if __name__ == "__main__":
     main()
